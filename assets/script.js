e7ec256dd2d24ebdf2b2ce3dda50da5284926c6f
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-28 21:51:19 +0100

    Start writing function that adds digits to display
    So far the function displays the numbers in the display.
    
    If a math symbol is clicked, the number in the display is stored
    in a variable called firstNumber.

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,28 @@ const display = document.querySelector(".display");
 
 const buttonsContainer = document.querySelector(".buttons-container");
 
+
 function addToDisplay(event) {
-    display.textContent = event.target.textContent;
+    let firstNumber;
+    if(
+        event.target.id === "0" ||
+        event.target.id === "1" ||
+        event.target.id === "2" ||
+        event.target.id === "3" ||
+        event.target.id === "4" ||
+        event.target.id === "5" ||
+        event.target.id === "6" ||
+        event.target.id === "7" ||
+        event.target.id === "8" ||
+        event.target.id === "9" 
+    )   {  
+            display.textContent += event.target.textContent; 
+        }
+
+    if(event.target.id === "+") {
+    firstNumber = display.textContent;
+    console.log(firstNumber);
+ }
 }
 
 buttonsContainer.addEventListener("click", addToDisplay);
