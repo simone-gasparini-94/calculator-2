d1f1899e952e70d2f8de91dd52cbcee6eb1c1b4a
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-28 19:17:42 +0100

    Add js variables and an event listener to the buttons container

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-console.log("Hello, world!")
+const display = document.querySelector(".display");
+
+const buttonsContainer = document.querySelector(".buttons-container");
+
+function addToDisplay(event) {
+    display.textContent = event.target.textContent;
+}
+
+buttonsContainer.addEventListener("click", addToDisplay);
