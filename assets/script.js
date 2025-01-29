8ef003f6f1381f95420c3d24591652621d6d26fe
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-29 11:12:35 +0100

    Add equals that stores value in secondNumber

--- assets/script.js
+++ assets/script.js
@@ -7,40 +7,39 @@ const calculator = {
     firstNumber: null,
     secondNumber: null,
     digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    mathSymbols: ["+", "−", "×", "÷"]
+    mathSymbols: ["+", "−", "×", "÷"],
+    equals: "=",
 }
 
 display.textContent = calculator.initialNumber;
 
-//function triggered by the buttons
 function addToDisplay(event) {
     //AC resets the display to the initial number (0)
-    if(
-        event.target.id === "AC"
-    )   {
+    if(event.target.id === "AC")   {
         display.textContent = calculator.initialNumber;
     }
 
-    if(
-        calculator.digits.includes(event.target.id)
-    )   {   
-            if(
-                display.textContent === calculator.initialNumber
-            ) {
-                display.textContent = event.target.textContent; 
-            } else {
-                display.textContent += event.target.textContent;
-            }
+    if(calculator.digits.includes(event.target.id))   {   
+        if(
+            display.textContent === calculator.initialNumber ||
+            calculator.mathSymbols.includes(display.textContent)
+        ) {
+            display.textContent = event.target.textContent; 
+        } else {
+            display.textContent += event.target.textContent;
         }
+    }
 
-    if(
-        calculator.mathSymbols.includes(event.target.id)
-    )   {
+    if(calculator.mathSymbols.includes(event.target.id))   {
+        calculator.firstNumber = +display.textContent;
+        console.log(calculator.firstNumber, typeof calculator.firstNumber);
+        display.textContent = event.target.textContent;
+    }
 
-            calculator.firstNumber = +display.textContent;
-            console.log(calculator.firstNumber, typeof calculator.firstNumber);
-            display.textContent = event.target.textContent;
-        }
+    if(event.target.id === calculator.equals) {
+        calculator.secondNumber = +display.textContent;
+        console.log(calculator.secondNumber, typeof calculator.secondNumber);
+    }
 }
 
 buttonsContainer.addEventListener("click", addToDisplay);
