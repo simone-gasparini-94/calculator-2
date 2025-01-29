907d7bcc8ce63088f59bfc5e4ba1736da594db7c
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-29 10:09:23 +0100

    Add if else condition that checks if the displayed number is 0

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,18 @@ const display = document.querySelector(".display");
 
 const buttonsContainer = document.querySelector(".buttons-container");
 
+let initialNumber = "0";
+display.textContent = initialNumber;
 
+//function triggered by the buttons
 function addToDisplay(event) {
-    let firstNumber;
+    //AC resets the display to the initial number (0)
+    if(
+        event.target.id === "AC"
+    )   {
+        display.textContent = initialNumber;
+    }
+
     if(
         event.target.id === "0" ||
         event.target.id === "1" ||
@@ -16,8 +25,13 @@ function addToDisplay(event) {
         event.target.id === "7" ||
         event.target.id === "8" ||
         event.target.id === "9" 
-    )   {  
-            display.textContent += event.target.textContent; 
+    )   {   
+            //if condition that deletes the first 0
+            if(display.textContent === initialNumber) {
+                display.textContent = event.target.textContent; 
+            } else {
+                display.textContent += event.target.textContent;
+            }
         }
 
     if(
@@ -27,7 +41,7 @@ function addToDisplay(event) {
         event.target.id === "÷"
     )   {
             firstNumber = display.textContent;
-            console.log(firstNumber);
+            display.textContent = firstNumber + " " + event.target.textContent;
         }
 }
 
