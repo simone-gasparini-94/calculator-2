a3e3a1476943af9e7f2e667b176aef3a9fd7f172
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-29 15:09:18 +0100

    Separate the logic of each button type into different functions
    Cleared the logic by creating smaller functions that have
    single responsibilities

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,66 @@
-const display = document.querySelector(".display");
-
-const buttonsContainer = document.querySelector(".buttons-container");
-
 const calculator = {
     initialNumber: "0",
-    firstNumber: null,
-    secondNumber: null,
     digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     mathSymbols: ["+", "−", "×", "÷"],
     equals: "=",
+
+    firstNumber: null,
+    secondNumber: null,
+    operator: null,
 }
 
+const display = document.querySelector(".display");
 display.textContent = calculator.initialNumber;
+const buttonsContainer = document.querySelector(".buttons-container");
 
-function addToDisplay(event) {
-    //AC resets the display to the initial number (0)
+// Main function: event handler of the clicks of the buttons
+// that incorporates the various functions
+
+function calculate(event) {
     if(event.target.id === "AC")   {
-        display.textContent = calculator.initialNumber;
+       resetDisplay();
     }
 
     if(calculator.digits.includes(event.target.id))   {   
-        if(
-            display.textContent === calculator.initialNumber ||
-            calculator.mathSymbols.includes(display.textContent)
-        ) {
-            display.textContent = event.target.textContent; 
-        } else {
-            display.textContent += event.target.textContent;
-        }
+        addDigit(event.target.id);
     }
 
     if(calculator.mathSymbols.includes(event.target.id))   {
-        calculator.firstNumber = +display.textContent;
-        console.log(calculator.firstNumber, typeof calculator.firstNumber);
-        display.textContent = event.target.textContent;
+        addOperator(event.target.id);
     }
 
     if(event.target.id === calculator.equals) {
-        calculator.secondNumber = +display.textContent;
-        console.log(calculator.secondNumber, typeof calculator.secondNumber);
+        addEquals();
     }
 }
 
-buttonsContainer.addEventListener("click", addToDisplay);
+function resetDisplay() {
+    display.textContent = calculator.initialNumber;
+}
+
+function addDigit(digit) {
+    if(
+        display.textContent === calculator.initialNumber ||
+        calculator.mathSymbols.includes(display.textContent)
+    ) {
+        display.textContent = digit; 
+    } else {
+        display.textContent += digit;
+    }
+}
+
+function addOperator(operator) {
+    calculator.firstNumber = +display.textContent;
+    console.log(calculator.firstNumber, typeof calculator.firstNumber);
+    display.textContent = operator;
+}
+
+function addEquals() {
+    calculator.secondNumber = +display.textContent;
+    console.log(calculator.secondNumber, typeof calculator.secondNumber);
+    calculator.firstNumber += calculator.secondNumber;
+    display.textContent = calculator.firstNumber;
+}
+
+
+buttonsContainer.addEventListener("click", calculate);
